f103c3a4f7a43dd9325798747a66126d308c42bd
Zheng Zhang <56539959+Adair-zz@users.noreply.github.com>
2022-02-22 13:02:57 +0800

    Add files via upload

---
+++ Tutorial 1.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+name = input("What''s your name ")
+
+
+# In[2]:
+
+
+age = input("How old are you: ")
+
+
+# In[3]:
+
+
+adress = input("Where are you living: ")
+
+
+# In[4]:
+
+
+sport = input("YOur favorite sport: ")
+
+
+# In[5]:
+
+
+print("Hello, my name is ", name)
+
+
+# In[6]:
+
+
+print("I am", age, " years")
+
+
+# In[7]:
+
+
+print("I live in", adress)
+
+
+# In[8]:
+
+
+print("I like to play ", sport)
+
